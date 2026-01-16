252f8b2e744700cc7fda1efff6cb37da986b437e
MARIA KANELLAKI <code.compile240@slmails.com>
2026-01-16 14:01:52 +0200

    counters

---
+++ src/objectCounters.cpp
@@ -0,0 +1,12 @@
+#include "TrafficLights.h"
+#include "StationaryVehicles.h"
+#include "TrafficSigns.h"
+#include "MovingBikes.h"
+#include "MovingCars.h"
+
+int TrafficLights::TrafficLightscount = 0;
+int StationaryVehicles::ParkedCarcount = 0;
+int TrafficSigns::TrafficSignscount = 0;
+int TrafficSigns::TrafficSignsSTOPcount = 0;
+int MovingBikes::MovingBikescount = 0;
+int MovingCars::MovingCarscount = 0;
