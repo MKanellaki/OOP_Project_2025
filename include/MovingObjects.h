2aafa01d1a9369652c46c9a76bf0455873db8c54
MARIA KANELLAKI <code.compile240@slmails.com>
2026-01-16 21:38:08 +0200

    moving objetcts comments

--- include/MovingObjects.h
+++ include/MovingObjects.h
@@ -5,9 +5,9 @@ class MovingObjects : public WorldObject{
 protected:
 string direction;
 int speed;
-bool in;
+bool in;//to see if it's in
 public:
-bool get_in(){
+bool get_in(){//Every get_.... return the corresponding variable
         return in;
     };
     void not_in(){
@@ -16,15 +16,15 @@ bool get_in(){
 string get_direction() const{
         return direction;
     };
-    void move(){
+    void move(){//based on the direction changing the x or y by the speed  to "move"
         int x=get_x_pos();
         int y=get_y_pos();
         if (direction=="left")x=x-speed;
         if (direction=="right")x=x+speed;
         if (direction=="up")y=y+speed;
         if (direction=="down")y=y-speed;
-        add_pos(x,y);
-        int f =rand()%(4);
+        add_pos(x,y);//updates the position using  x and y
+        int f =rand()%(4);//gets a random number and changes the direction based on it
         if (f==0)direction="left";
         if (f==1)direction="right";
         if (f==2)direction="up";
