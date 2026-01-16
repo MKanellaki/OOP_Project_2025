8d6b56552908342d822c063633f3846a9ab4ab75
MARIA KANELLAKI <code.compile240@slmails.com>
2026-01-16 21:41:40 +0200

    moving cars comments

--- include/MovingCars.h
+++ include/MovingCars.h
@@ -6,19 +6,18 @@ class MovingCars : public MovingObjects{
     static int MovingCarscount;
     public:
     MovingCars(){
-        MovingCarscount++;
-        in =true;
+        MovingCarscount++;//counts the objects that exist of tha type
+        in =true;//sets everything with the default variables
         glyph ="C";
         ID.ID_string="MovingCar:";
         Type=ObjectType::MOVING_CAR ;
         ID.ID_int=MovingCarscount;
         speed=2;
-        int f =rand()%(4);
+        int f =rand()%(4);//gets a random number and puts a direction based on it
         if (f==0)direction="left";
         if (f==1)direction="right";
         if (f==2)direction="up";
         if (f==3)direction="down";
-      //  cout <<direction <<"\n"<<endl;
     };
     void describe()override{
        cout <<"MovingCars direction\n"<<endl;
