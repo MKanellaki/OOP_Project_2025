4d10dea18fb708554a4f0125253bf1f15a4177d8
dinosrant <root@DESKTOP-9670TQD.localdomain>
2026-01-16 15:01:50 +0200

    simulation.cpp

--- simulation.cpp
+++ simulation.cpp
@@ -8,9 +8,15 @@ World.set_dimX(10);
 World.set_simulationTicks(10);
 World.create_GridWorld();
 World.visualization_full(make_tuple(0,0),"@");
+    srand(World.get_seed());
 for(int i=1;i<10;i++){
     World.NEW_tick();
-World.visualization_pov(make_tuple(i,i),"@",5);
+
+      int f =rand()%(4);
+        if (f==0)World.visualization_pov_front(make_tuple(i,i),"@",Direction::NORTH);
+        if (f==1)World.visualization_pov_front(make_tuple(i,i),"@",Direction::EAST);
+        if (f==2)World.visualization_pov_front(make_tuple(i,i),"@",Direction::WEST);
+        if (f==3)World.visualization_pov_front(make_tuple(i,i),"@",Direction::SOUTH);
 };
 World.visualization_full(make_tuple(9,9),"@");
 };
